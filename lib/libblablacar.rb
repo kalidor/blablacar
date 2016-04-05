eb27288637dfb0b6260a09c0e79461aa8dbf5351
kalidor <kalidor@unixed.fr>
2016-04-05 10:54:00 +0200

    Look for old trip, useful for duplicate

--- lib/libblablacar.rb
+++ lib/libblablacar.rb
@@ -761,7 +761,7 @@ class Blablacar
       trip_dupl_req = setup_http_request($duplicate_active_trip_offers, @cookie)
     else
       $duplicate_inactive_trip_offers[:data] = data.join("&")
-      $duplicate_inactive_trip_offers[:url] += "?page=#{indx+1}"
+      $duplicate_inactive_trip_offers[:url] += "?page=#{indx}"
       trip_dupl_req = setup_http_request($duplicate_inactive_trip_offers, @cookie)
     end
     res = @http.request(trip_dupl_req)
